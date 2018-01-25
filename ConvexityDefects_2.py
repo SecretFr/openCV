bcc546de93f69ba36969919c8a759a46134d8503
SecretFr <linkclean@naver.com>
2018-01-25 09:13:37 +0900

    Distance between Points

---
+++ ConvexityDefects_2.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+
+def contour():
+    img = cv2.imread('images/star.jpg')
+    imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    ret, thr = cv2.threshold(imgray, 127, 255, 0)
+    _, contours, _ = cv2.findContours(thr, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    cnt = contours[0]
+    cv2.drawContours(img, [cnt], 0, (0, 255, 0), 2)
+
+    outside = (55, 70)
+    inside = (140, 150)
+
+    dist1 = cv2.pointPolygonTest(cnt, outside, True)
+    dist2 = cv2.pointPolygonTest(cnt, inside, True)
+    #3번째 인자가 True로 설정되면 Contour와 점 사이의 최단 거리를 리턴한다.
+    #False로 설정되면 최단 거리를 리턴하는 것이 아니라, 주어진 점이 Contour의 외부에 있으면 -1
+    #Contour위에 있으면 0, Contour 내부에 있으면 1을 리턴한다.
+    #따라서 True인 경우와 비교해 계산 시간이 2~3배 빠르다.
+    #거리값이 필요하지 않다면 3번째 인자를 항상 false로 두는 것이 좋다.
+
+    print('Contour에서 (%d, %d)까지 거리: %.3f' %(outside[0], outside[1], dist1))
+    print('Contour에서 (%d, %d)까지 거리: %.3f' %(inside[0], inside[1], dist2))
+
+    cv2.circle(img, outside, 3, (0, 255, 0), -1)
+    cv2.circle(img, inside, 3, (255, 0, 255), -1)
+
+    cv2.imshow('defects', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+contour()
